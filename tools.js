b84cb1d5fc3ace96e5a78a13f7d45cc962aacdd4
tsantos301 <tsantos@ualberta.ca>
2019-01-08 16:29:15 -0700

    First three converters working

--- tools.js
+++ tools.js
@@ -54,19 +54,19 @@ function convertWeight(){
 
 function convertArea(){
 
-    var startUnit = document.getElementById( "startLengthUnit" ).value;
-    var endUnit = document.getElementById( "endLengthUnit" ).value;
-    var startValue = document.getElementById( "startLength" ).value;
+    var startUnit = document.getElementById( "startAreaUnit" ).value;
+    var endUnit = document.getElementById( "endAreaUnit" ).value;
+    var startValue = document.getElementById( "startArea" ).value;
     //console.log(startUnit);
-    if(startUnit == "meter" && endUnit == "meter") document.getElementById( "endLength" ).value = startValue;
-    if(startUnit=="meter" && endUnit=="centimeter") document.getElementById( "endLength" ).value = startValue*100;
-    if(startUnit=="meter" && endUnit=="kilometer") document.getElementById( "endLength" ).value = startValue/1000;
-    if(startUnit == "kilometer" && endUnit == "kilometer") document.getElementById( "endLength" ).value = startValue;
-    if(startUnit=="kilometer" && endUnit=="meter") document.getElementById( "endLength" ).value = startValue*1000;
-    if(startUnit == "kilometer" && endUnit == "centimeter") document.getElementById( "endLength" ).value = startValue*100000;
-    if(startUnit == "centimeter" && endUnit == "centimeter") document.getElementById( "endLength" ).value = startValue;
-    if(startUnit=="centimeter" && endUnit=="meter") document.getElementById( "endLength" ).value = startValue/100;
-    if(startUnit == "centimeter" && endUnit == "kilometer") document.getElementById( "endLength" ).value = startValue/100000;
+    if(startUnit == "squareMeter" && endUnit == "squareMeter") document.getElementById( "endArea" ).value = startValue;
+    if(startUnit=="squareMeter" && endUnit=="squareMile") document.getElementById( "endArea" ).value = startValue/2.59e+6;
+    if(startUnit=="squareMeter" && endUnit=="squareKilometer") document.getElementById( "endArea" ).value = startValue/1e+6;
+    if(startUnit == "squareKilometer" && endUnit == "squareKilometer") document.getElementById( "endArea" ).value = startValue;
+    if(startUnit=="squareKilometer" && endUnit=="squareMeter") document.getElementById( "endArea" ).value = startValue*1e+6;
+    if(startUnit == "squareKilometer" && endUnit == "squareMile") document.getElementById( "endArea" ).value = startValue/ 2.59;
+    if(startUnit == "squareMile" && endUnit == "squareMile") document.getElementById( "endArea" ).value = startValue;
+    if(startUnit=="squareMile" && endUnit=="squareMeter") document.getElementById( "endArea" ).value = startValue* 2.59e+6;
+    if(startUnit == "squareMile" && endUnit == "squareKilometer") document.getElementById( "endArea" ).value = startValue*2.59;
 }
 
 
